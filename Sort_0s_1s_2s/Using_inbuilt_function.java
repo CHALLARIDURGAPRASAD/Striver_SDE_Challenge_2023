fd839432f685b9f9d727cfb1bb8850bc593bf4e2
CHALLARI DURGAPRASAD <87119929+CHALLARIDURGAPRASAD@users.noreply.github.com>
2023-06-06 12:19:55 +0530

    Create Using_inbuilt_function.java

---
+++ Sort_0s_1s_2s/Using_inbuilt_function.java
@@ -0,0 +1,14 @@
+import java.util.* ;
+import java.io.*; 
+public class Solution 
+{
+    public static void sort012(int[] arr)
+    {
+      Arrays.sort(arr);    
+    }
+}
+/*
+Note:- Instead of using inbuilt function you can use merge sort and any other sorting algorithms.
+TIME COMPLEXITY:- O(NlogN)
+SPACE COMPLEXITY:- O(1)
+*/
