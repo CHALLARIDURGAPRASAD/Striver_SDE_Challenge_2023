ab85e9f27b3fdbcbc75d4407f9a589db865dea4e
CHALLARI DURGAPRASAD <87119929+CHALLARIDURGAPRASAD@users.noreply.github.com>
2023-06-07 10:55:53 +0530

    Create Using_linear_search.java

---
+++ Finding_element_in_matrix/Using_linear_search.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+public class Solution {
+    static boolean searchMatrix(ArrayList<ArrayList<Integer>> mat, int target) {
+        // Write your code here.
+        for(int i=0;i<mat.size();i++){
+            for(int j=0;j<mat.get(i).size();j++)
+            {
+                if(mat.get(i).get(j)==target)
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+/*
+TIME COMPLEXITY:- O(N*M)
+SPACE COMPLEXITY:- O(1)
+*/
